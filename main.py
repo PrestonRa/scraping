6f395e4927bde13c0aa0892b196fc57d2f47ba01
Preston <preston.ramrattan@gmail.com>
2024-01-31 14:48:36 +0900

    finished all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-
+import sqlite3
 import requests
 import selectorlib
 import smtplib
@@ -10,49 +10,74 @@ import ssl
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-def scrape(url): #scrape the page source from the url
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
 
-def send_email(message, subject):
-    host = "smtp.gmail.com"
-    port = 465
 
-    username = "gmailaddress@"
-    password = "special gmail password"
-    receiver = "gmailaddress@"
-    context = ssl.create_default_context()
+class Event:
+    def scrape(self, url): #scrape the page source from the url
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Email was sent!")
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
-def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
 
-def read(extracted):
-    with open("data.txt", 'r') as file:
-        return file.read()
+class Email:
+    def send(self, message):
+        host = "smtp.gmail.com"
+        port = 465
+
+        username = "gmailaddress@"
+        password = "special gmail password"
+        receiver = "gmailaddress@"
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("Email was sent!")
+
+
+class Database:
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
+
+    def store(self,extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
+
+    def read(self,extracted):
+        row = extracted.split(',')
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
+
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
-        content = read(extracted)
+
         if extracted != "No upcoming tours":
-            if extracted not in content:
-                store(extracted)
-                send_email(message="Hey, new event was found!", subject="Check it out!")
-        time.sleep(2) # How often to run the script eg: every two seconds
+            database = Database(database_path="data.db")
+            row = database.read(extracted)
+            if not row:
+                database.store(extracted)
+                email = Email()
+               # email.send(message="Hey, new event was found!", subject="Check it out!")
+        time.sleep(10) # How often to run the script eg: every two seconds
 
 # Added subject to def send_email, not sure if that works
